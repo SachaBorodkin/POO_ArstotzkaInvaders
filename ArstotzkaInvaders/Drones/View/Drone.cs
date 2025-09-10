379cae22a1265dfc76bb2f7ab2109b3e9a369a6d
Borodkin Oleksandr Stepanovych <150446572+SachaBorodkin@users.noreply.github.com>
2025-09-10 16:31:33 +0200

    feat(ArstotzkaInvaders) ajoute de la base de jeu avec le drone avec une texture qui bouge

---
+++ ArstotzkaInvaders/Drones/View/Drone.cs
@@ -0,0 +1,20 @@
+﻿using Drones.Helpers;
+using Drones.Properties;
+using System.Resources;
+
+namespace Drones
+{
+    // Cette partie de la classe Drone définit comment on peut voir un drone
+
+    public partial class Drone
+    {
+        private Pen droneBrush = new Pen(new SolidBrush(Color.Purple), 3);
+
+        // De manière graphique
+        public void Render(BufferedGraphics drawingSpace)
+        {
+            drawingSpace.Graphics.DrawImage(Resources.drone, X, Y, 140, 70);
+        }
+
+    }
+}
