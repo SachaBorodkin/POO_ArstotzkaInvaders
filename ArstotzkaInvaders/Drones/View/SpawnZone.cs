aa06e06cc1948cffe4eee383e76bf7ec357cc0f1
Borodkin Oleksandr Stepanovych <150446572+SachaBorodkin@users.noreply.github.com>
2025-09-17 16:33:04 +0200

    feat(drones) ajout de la zone de spawn des ennemies

---
+++ ArstotzkaInvaders/Drones/View/SpawnZone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drones.View
+{
+     class SpawnZone
+    {
+        private const int _ZONE_HEIGHT = 500;
+        private const int _ZONE_WIDTH = 500;
+    }
+}
